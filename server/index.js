ff8dfec63b430e628861e12760565ff7c1dec8e3
LeTruongThinh-07052007 <Let242344@gmail.com>
2024-09-04 15:23:19 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: ["http://localhost:3000", "http://localhost:5173","https://slumbershift.netlify.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
